0bd64320915eb1fc8009aee636c73b72cf99da40
newhorizon-tech <alaukikinbox@gmail.com>
2021-09-17 18:51:00 +0530

    Fix Linters

--- index.js
+++ index.js
@@ -1,74 +1,67 @@
 let bookList = [];
 
 const saveData = (data) => {
-  console.log("Data Save" + data)
-  localStorage.setItem("bookList", JSON.stringify(data));
-}
+  localStorage.setItem('bookList', JSON.stringify(data));
+};
 
+const addBtn = document.querySelector('#addButton');
+const titleIn = document.querySelector('#title-input');
+const authorIn = document.querySelector('#author-input');
 
-const addBtn = document.querySelector("#addButton");
-const titleIn = document.querySelector("#title-input");
-const authorIn = document.querySelector("#author-input");
-
-const bookDisplay = document.querySelector("#book-display");
-
-const deleteBook = (e) => {
-  const bookElement = e.target.parentElement;
-  const title = bookElement.querySelector(".book-title").textContent
-  const author = bookElement.querySelector(".book-author").textContent
-  bookList = bookList.filter((book) => {
-    return !(book.title === title && book.author === author)
-  })
-
-  displayBooks()
-}
-
+const bookDisplay = document.querySelector('#book-display');
 
 const displayBooks = () => {
-  bookDisplay.innerHTML = "";
-  bookList.forEach(book => {
-    const title = document.createElement("p");
-    const author = document.createElement("p");
+  bookDisplay.innerHTML = '';
+  bookList.forEach((book) => {
+    const title = document.createElement('p');
+    const author = document.createElement('p');
 
-
-    title.className = "book-title";
+    title.className = 'book-title';
     title.textContent = book.title;
-    author.className = "book-author";
+    author.className = 'book-author';
     author.textContent = book.author;
-    const bookElement = document.createElement("div");
-    bookElement.className = "book-element";
+    const bookElement = document.createElement('div');
+    bookElement.className = 'book-element';
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Remove Book"
-    deleteButton.addEventListener("click", e => deleteBook(e))
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Remove Book';
+    deleteButton.addEventListener('click', (e) => deleteBook(e)); // eslint-disable-line
     bookElement.append(title, author, deleteButton);
 
     bookDisplay.append(bookElement);
-  })
+  });
   saveData(bookList);
-}
+};
+
+const deleteBook = (e) => {
+  const bookElement = e.target.parentElement;
+  const title = bookElement.querySelector('.book-title').textContent;
+  const author = bookElement.querySelector('.book-author').textContent;
+  bookList = bookList.filter((book) => !(book.title === title && book.author === author));
+  displayBooks();
+};
 
 const bookInput = () => {
   bookList.push({
     title: titleIn.value,
-    author: authorIn.value
+    author: authorIn.value,
   });
 
-  titleIn.textContent = "";
-  authorIn.textContent = "";
-  displayBooks()
-}
+  titleIn.textContent = '';
+  authorIn.textContent = '';
+  displayBooks();
+};
 
-addBtn.addEventListener("click", bookInput)
+addBtn.addEventListener('click', bookInput);
 
 const loadData = () => {
-  let data = localStorage.getItem("bookList");
+  const data = localStorage.getItem('bookList');
   if (data != null) {
     bookList = JSON.parse(data);
-    displayBooks()
+    displayBooks();
   }
-}
+};
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    loadData();
+document.addEventListener('DOMContentLoaded', () => {
+  loadData();
 });
