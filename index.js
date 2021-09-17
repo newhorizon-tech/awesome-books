1f678010c81b8c28985fd697623ba10366c9f3db
newhorizon-tech <alaukikinbox@gmail.com>
2021-09-17 17:52:04 +0530

    Restructure bookList array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Javascript!");
 
+const bookList = [];
+
 const addBtn = document.querySelector("#addButton");
 const titleIn = document.querySelector("#title-input");
 const authorIn = document.querySelector("#author-input");
@@ -8,23 +10,34 @@ const bookDisplay = document.querySelector("#book-display");
 
 console.log(bookDisplay);
 
+
+
+const displayBooks = () => {
+  bookDisplay.innerHTML = "";
+  bookList.forEach( book => {
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    const bookElement = document.createElement("div");
+    bookElement.className = "book-element";
+
+    const deleteButton = document.createElement("button");
+    bookElement.append(title, author)
+
+    bookDisplay.append(bookElement);
+  })
+  console.log(bookList);
+}
+
 const bookInput =  () => {
-  const title = document.createElement("p");
-  const author = document.createElement("p");
 
-  title.textContent = titleIn.value;
-  author.textContent = authorIn.value;
-
-  console.log(titleIn.value)
+  bookList.push({title: titleIn.value, author: authorIn.value});
 
   titleIn.textContent = "";
   authorIn.textContent = "";
-  const bookElement = document.createElement("div");
-  bookElement.className = "book-element";
-  bookElement.append(title, author)
-
-  bookDisplay.append(bookElement);
-
+  displayBooks()
 }
 
 addBtn.addEventListener("click", bookInput)
